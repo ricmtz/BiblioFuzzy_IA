b3d37c93fdb7f61a43e23bac316566de2d644992
ricmtz <ricmtz15@gmail.com>
2017-11-02 16:35:23 -0600

    geracion de recomendaciones

--- Main.py
+++ Main.py
@@ -2,9 +2,11 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+import sistema_fuzzy_juegos as sfj
+
 class Main(QtWidgets.QWidget):
     def __init__(self):
-        super().__init__()
+        super().__init__()        
         self.__duracion = 0
         self.__estrategia = 0
         self.__accion = 0
@@ -65,6 +67,7 @@ class Main(QtWidgets.QWidget):
         violenci_sld.valueChanged.connect(self.val_violencia)
 
         calc_rec_btn = QtWidgets.QPushButton("Generar")
+        calc_rec_btn.clicked.connect(self.generar_recomendacion)
 
         duracion_vbox.addWidget(duracion_sld)
         duracion_gbox.setLayout(duracion_vbox)     
@@ -105,7 +108,39 @@ class Main(QtWidgets.QWidget):
         self.__exploracion = valor_e
     
     def val_violencia(self, valor_v):
-        self.__violencia = valor_v    
+        self.__violencia = valor_v
+    
+    def generar_recomendacion(self):
+        __fuzzy = sfj.SistemaFuzzyJuegos()
+        duracion_df = __fuzzy.fuzzificador_duracion(self.__duracion)
+        estrategia_df = __fuzzy.fuzzificador_estrategia(self.__estrategia)
+        accion_df = __fuzzy.fuzzificador_accion(self.__accion)
+        dificultad_df = __fuzzy.fuzzificador_dificultad(self.__dificultad)
+        exploracion_df = __fuzzy.fuzzificador_exploracion(self.__exploracion)
+        sangre_df = __fuzzy.fuzzificador_sangre(self.__violencia)
+        genero = __fuzzy.inferir_atributos_difusos_cualitativos(duracion_df, estrategia_df,
+                                                                     accion_df, dificultad_df, 
+                                                                     exploracion_df, sangre_df)
+        if genero == "RPG":
+            msg_rpg = ("El genero de juegos que te recomendamos es RPG.\n" +
+                    "Te recomendamos:\n" +  "\t-Final Fantasy\n"+ "\t-The Legend of Zelda\n" +
+                    "\t-Skyrim\n" + "\t-Fire Emblem" )
+            mb_rpg = QtWidgets.QMessageBox.information(self, "Recomendacion", msg_rpg, QtWidgets.QMessageBox.Ok)            
+        elif genero == "Plataformas":
+            msg_plataform = ("El genero de juegos que te recomendamos es Plataformas.\n" +
+                    "Te recomendamos:\n" +  "\t-Super Mario Bros\n"+ "\t-Megaman\n" +
+                    "\t-Metroid\n" + "\t-Castelvania" )
+            QtWidgets.QMessageBox.information(self, "Recomendacion", msg_plataform, QtWidgets.QMessageBox.Ok)
+        elif genero == "Peleas":
+            msg_peleas = ("El genero de juegos que te recomendamos es Peleas.\n" +
+                    "Te recomendamos:\n" +  "\t-Street Fighter\n"+ "\t-God of War\n" +
+                    "\t-Castle Crasher\n" + "\t-Devil May Cry" )
+            QtWidgets.QMessageBox.information(self, "Recomendacion", msg_peleas, QtWidgets.QMessageBox.Ok)
+        elif genero == "Disparos":
+            msg_disparos = ("El genero de juegos que te recomendamos es Disparos.\n" +
+                    "Te recomendamos:\n" +  "\t-Overwatch\n"+ "\t-Battlefield 1\n" +
+                    "\t-Enter the Gungeon\n" + "\t-Gears of war" )
+            QtWidgets.QMessageBox.information(self, "Recomendacion", msg_disparos, QtWidgets.QMessageBox.Ok)
 
 if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
